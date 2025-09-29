794c702f4d543fa816d729cd62a635944a710080
Kovács Tibor <kovacs.tibor@diak.szbi-pg.hu>
2025-09-29 07:37:23 +0200

    Update carrental.sql

--- carrental.sql
+++ carrental.sql
@@ -5,5 +5,19 @@ COLLATE utf8mb4_general_ci;
 USE Carrental;
 
 CREATE TABLE cars(
-    
+    car_id INT auto_increment PRIMARY KEY,
+
+);
+
+CREATE TABLE motorcycles(
+
+)
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT, -- Egyedi azonosító az ügyfélhez
+    first_name VARCHAR(50) NOT NULL, -- Ügyfél keresztneve
+    last_name VARCHAR(50) NOT NULL, -- Ügyfél vezetékneve
+    phone_number VARCHAR(20), -- Telefonszám
+    email VARCHAR(100) UNIQUE, -- E-mail cím
+    driver_license VARCHAR(50) UNIQUE NOT NULL -- Jogosítvány száma
 );
